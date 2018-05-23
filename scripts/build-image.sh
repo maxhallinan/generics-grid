48ebccd79127ae6c57020287f80c2a379d26f479
Max Hallinan <maxhallinan@riseup.net>
2018-05-23 16:51:39 +0200

    Tag Docker image Push to remote repo

--- scripts/build-image.sh
+++ scripts/build-image.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
-docker build --tag generics-travel .
+LOCAL_TAG="generics-travel:$(git rev-parse --short HEAD)"
+REMOTE_TAG="maxhallinan/$LOCAL_TAG"
+docker build --tag $LOCAL_TAG .
+docker tag $LOCAL_TAG $REMOTE_TAG
+docker push $REMOTE_TAG
