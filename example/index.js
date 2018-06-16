f8ab5c0f02ed626623cbf0417d33ca9843a63ad3
Max Hallinan <maxhallinan@riseup.net>
2018-06-16 12:34:09 +0200

    Change location of remote server in example/index.js

--- example/index.js
+++ example/index.js
@@ -1,7 +1,7 @@
 // eslint-disable-next-line no-console
 const log = (...args) => console.log(...args);
-// const ws = new WebSocket(`ws://generics-travel-dev.us-east-1.elasticbeanstalk.com:80`);
-const ws = new WebSocket(`ws://localhost:8080`);
+// const ws = new WebSocket(`ws://localhost:8080`);
+const ws = new WebSocket(`ws://generics.maxhallinan.com/travel`);
 ws.onopen = () => log(`Opening connection`);
 ws.onerror = (err) => log(`Error: ${err.message}`);
 ws.onmessage = (msg) => log(`Message received:`, JSON.parse(msg.data));
