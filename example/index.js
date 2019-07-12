59870d270551181447a7d7625f97d283251286c4
Max Hallinan <maxhallinan@riseup.net>
2019-07-12 15:04:36 -0400

    Save changes

--- example/index.js
+++ example/index.js
@@ -1,7 +1,7 @@
 // eslint-disable-next-line no-console
 const log = (...args) => console.log(...args);
-// const ws = new WebSocket(`ws://localhost:8080`);
-const ws = new WebSocket(`ws://generics.maxhallinan.com/travel`);
+const ws = new WebSocket(`ws://localhost:3000/travel`);
+// const ws = new WebSocket(`ws://generics.maxhallinan.com/travel`);
 ws.onopen = () => log(`Opening connection`);
 ws.onerror = (err) => log(`Error: ${err.message}`);
 ws.onmessage = (msg) => log(`Message received:`, JSON.parse(msg.data));
--- scripts/run-image.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-source .env
-
-docker run \
-  --env-file .env \
-  --expose $WS_PORT \
-  --publish $HOST_PORT:$WS_PORT \
-  maxhallinan/generics-travel
